a0a3d4bfad158874e7c01e38e6d4740b0c4ee3e9
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-10-10 21:38:15 -0700

    additional refactoring to server. drafted in handlers for validation logic and construction of computed TSFA and RRSP amounts

--- server/calculator.js
+++ server/calculator.js
@@ -1,16 +1,39 @@
 function calculate(req, res){
-    const input = req.body
+    const input = new CalculatorInput(req.body)
   try {
     validate(input)
-    res.send({TSFA: 1234.45, RRSP: 2345.23})
+    const result = {
+      TSFA: computeTSFA(input),
+      RRSP: computeRRSP(input)
+    }
+    res.send(result)
   }catch(validationError){
       res.status().send(validationError)
   }
 }
 
+function computeTSFA(input){
+  return 5
+}
+
+function computeRRSP(input){
+ return 6
+}
+
 function validate(){
+  //check iffields null; if fields are numeric values, if fields are decimals within expected rate, etc.
   var validationErrors = []
   //validate each field
+}
+
+class CalculatorInput{
+  constructor(data){
+    this.currRate = data.currRate || null
+    this.amount = data.amount || null
+    this.retireRate = data.retireRate || null
+    this.growthRate = data.growthRate || null
+    this.inflationRate = data.inflationRate || null
+  }
 }
 
 
