1d7f4a7b41e443a71f3f00986495f1fc2a1f948a
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-10-14 08:37:30 -0700

    added comments to serverside code

--- server/financialCalculator.js
+++ server/financialCalculator.js
@@ -5,7 +5,7 @@ import {roundTo} from "./util"
 export function handler(req, res){
   try {
     const result = calculate(new CalculatorInput(req.body))
-    res.send(result) //may cause an error if we iterate over-- a class?
+    res.send(result)
   }catch(error){
       res.status(400).send(error.message)
   }
@@ -13,6 +13,8 @@ export function handler(req, res){
 
 /*
   @param {CalculatorInput} input
+  @return {CalculatorOutput}
+  throws error if validation on calculatorInput fails.
  */
 export function calculate(calculatorInput){
   validate(calculatorInput)
@@ -24,13 +26,14 @@ export function calculate(calculatorInput){
 
 /*
 @param {CalculatorInput} input
+ throws error if validation on calculatorInput fails.
  */
 function validate(input){
   const validationErrors = []
   for(const field in input){
     if(!input.hasOwnProperty(field)) continue; //only interested in fields defined on CalculatorInput class.
     const value = input[field]
-    if(value === null){ //strict check on null because 0 is allowed.
+    if(value === null){ //strict check on null because 0 is acceptable value.
       validationErrors.push({field, message: "is required and must be a number."})
     }
     else {
@@ -39,7 +42,7 @@ function validate(input){
         case "retirementTaxRate":
         case "inflationRate":
         case "investmentGrowthRate":
-          if(Math.abs(value) > 100) {
+          if(Math.abs(value) > 100) { //since 'deflation', tax benefits, can result in negative rates, negatives are permissable and must check that abs. value doesn't exceed 100.
             validationErrors.push({field, message: "cannot exceed 100."})
           }
           break;
@@ -58,24 +61,45 @@ function validate(input){
 
 /*
  @param {CalculatorInput} input
+ @return {AccountResults}
  */
 export function computeTSFA(input){
-  return composeResults(input, (amountInvested)=>amountInvested, ()=>0)
+  return composeResults(input,
+    (amountInvested)=>amountInvested, //TSFA deposits made with after-tax dollars, so net cost is just the amount the user invests
+    ()=>0 //TSFA withdrawals are not taxed, so the tax on withdrawal is just 0.
+  )
+
+
 }
 
 /*
  @param {CalculatorInput} input
+ @return {AccountResults}
  */
 export function computeRRSP(input){
   return composeResults(input,
-    (amountInvested, taxRate)=>amountInvested/(1-taxRate),
-    (amount, taxRate)=>amount * taxRate)
+    (amountInvested, taxRate)=>amountInvested/(1-taxRate), //for the comparison to work, need to equate the -net cost to user- of depositing to the TSFA and RRSP. Because the net cost to user for the TSFA deposit equals the amount invested,
+    //need to adjust amount deposited into RRSP so that (taking the deducted refund into account) the net out of pocket cost to user equals the TSFA deposit.
+    /*
+    (small justification for why this value- the required after tax RRSP deposit- equals amountInvested/(1-taxRate))
+     RRSPNetCost(amountInvested) = afterTaxDeposit - taxRate*afterTaxDeposit (since they receive the refund on the deposit)
+     want RRSPNetCost(amountInvested) = TSFANetCost(amountInvested) = amountInvested (from the input field)
+     substituting "afterTaxDeposit" for amountInvested/(1-taxRate):
+
+     amountInvested/(1-taxRate) - amountInvested/(1-taxRate)*taxRate =
+     amountInvested - amountInvested*taxRate/(1-taxRate) =
+     amountInvested(1-taxRate)/(1-taxRate) =
+     amountInvested =
+     TSFANetCost(amountInvested)
+     */
+    (amount, taxRate)=>amount * taxRate) //RRSP withdrawals are taxed according to the retirement tax rate
 }
 
 /*
  @param {CalculatorInput} input
- @param {function(number, number)=>number} computeAfterTax
- @param {function(number, number)=>number} computeAmountTaxedOnWithdrawal
+ @param {function(number, number)=>number} computeAfterTax -> a function that returns the 'after tax' deposited into account given the net cost of investment to user and the user's current tax rate.
+ @param {function(number, number)=>number} computeAmountTaxedOnWithdrawal -> a function that computes the amount taxed on the withdrawn investment given the investment future value and estimated tax rate on withdrawal
+ @return {AccountResults}
  */
 export function composeResults(input, computeAfterTax, computeAmountTaxedOnWithdrawal){
   const nominalRateOfReturn = input.investmentGrowthRate/100
