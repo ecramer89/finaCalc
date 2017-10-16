e5ce1796cfe1f900c929efb7613804f44daa5dc1
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-10-16 07:57:07 -0700

    added some comments to calculator output contract explaining rationale behind more lenient Number.is finite check

--- server/contracts/CalculatorOutput.js
+++ server/contracts/CalculatorOutput.js
@@ -18,6 +18,10 @@ export class AccountResults{
    */
   constructor({afterTax,futureValue,amountTaxedOnWithdrawal,afterTaxFutureValue}){
       this.afterTax = Number.isFinite(afterTax) ? afterTax : "Too much to count."
+      //since arguments come from server, can trust that if argument is Infinity or NaN it's because the number is too big
+      //to support normal arithmetic (i.e., Infinity-infinity = NaN).
+      //as opposed to calculator input, where values that fail the Number.isFinite check might just be invalid, since they
+      //come from client.
       this.futureValue = Number.isFinite(futureValue) ? futureValue : "Too much to count."
       this.amountTaxedOnWithdrawal = Number.isFinite(amountTaxedOnWithdrawal) ? amountTaxedOnWithdrawal : "Too much to count."
       this.afterTaxFutureValue = Number.isFinite(afterTaxFutureValue) ? afterTaxFutureValue : "Too much to count."
